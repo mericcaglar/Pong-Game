0abe432517d1db4599e55603f75c88ce97528fe4
Mehmet Mert Gündüz <mehmetmertgunduz@ogrenci.beykoz.edu.tr>
2021-01-28 19:47:22 +0300

    Topun oyunculardan sekmesi yapıldı

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -24,4 +24,12 @@ public class BallController : MonoBehaviour
         rb.velocity = direction * speed;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            direction.x = -direction.x;
+        }
+        
+    }
 }
