abcd3c7e0d5b3a101871c0347d3085e9d7485592
Mehmet Mert Gündüz <mehmetmertgunduz@ogrenci.beykoz.edu.tr>
2021-01-28 19:29:41 +0300

    top hareketi yapıldı

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]private float speed;
+
+    private Vector2 direction;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = new Vector2(Random.Range(-1,1),Random.Range(-1,1));
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = direction * speed;
+    }
+
+}
