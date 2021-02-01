6880b8a8a253d98348d0de506876d313bb3b3957
Meriç Çağlar <mrcaglar6@gmail.com>
2021-02-02 01:08:00 +0300

    Add Some Skill

--- Assets/Scripts/ApplySkill.cs
+++ Assets/Scripts/ApplySkill.cs
@@ -21,19 +21,35 @@ public class ApplySkill : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            //Çubukların boyunu değiştirir
             if (getColor == "RGBA(0.557, 0.267, 0.678, 1.000)")
             {
                 playerObj.transform.localScale = new Vector3(0.42f,5f,1);
                 StartCoroutine(resetWidth(playerObj));
                 
             }
+            //Topun boyunu değiştirir
+            if (getColor == "RGBA(0.750, 0.220, 0.160, 1.000)")
+            {
+                this.gameObject.transform.localScale = new Vector3(1f, 1f, 1);
+                StartCoroutine(resetWidth(this.gameObject));
+                
+            }
         }
         if (collision.gameObject.CompareTag("Computer"))
         {
+            //Çubukların boyunu değiştirir
             if (getColor == "RGBA(0.557, 0.267, 0.678, 1.000)")
             {
                 computerObj.transform.localScale = new Vector3(0.42f,5f,1);
                 StartCoroutine(resetWidth(computerObj));
+            }
+            //Topun boyunu değiştirir
+            if (getColor == "RGBA(0.750, 0.220, 0.160, 1.000)")
+            {
+                this.gameObject.transform.localScale = new Vector3(1f,1f,1);
+                StartCoroutine(resetWidth(this.gameObject));
+                
             }
         }
     }
@@ -41,6 +57,13 @@ public class ApplySkill : MonoBehaviour
     IEnumerator resetWidth(GameObject gameObject )
     {
         yield return new WaitForSeconds(4);
-        gameObject.transform.localScale = new Vector3(0.42f,3f,1);
+        if (gameObject != this.gameObject)
+        {
+            gameObject.transform.localScale = new Vector3(0.42f,3f,1); 
+        }
+        else
+        {
+            this.gameObject.transform.localScale = new Vector3(0.5f,0.5f,1);
+        }
     }
 }
